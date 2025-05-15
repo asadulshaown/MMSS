6e75b540d73a21973c85c3ccde2b763b5d74177e
asa <asa27403@gmail.com>
2025-05-16 02:00:07 +0600

    fix some bugs

--- script.js
+++ script.js
@@ -1,90 +1,100 @@
 
 
 
-const form = document.querySelector("form") //form tag find
-const [name,monuthName,address,managerName] = form.querySelectorAll("input")//Destructuring 
+const form = document.querySelector("form"); //form tag find
+const [name,monuthName,address,managerName] = form.querySelectorAll("input");//Destructuring 
 
-const heading_1 = document.getElementsByClassName('heading_1')// name h2 tag find
-const addName = heading_1[0]
+const heading_1 = document.getElementsByClassName('heading_1');// name h2 tag find
+const addName = heading_1[0];
 
-const heading_2 = document.getElementsByClassName('heading_2')//address h2 tag find
-const addMounthName=heading_2[0]
+const heading_2 = document.getElementsByClassName('heading_2');//address h2 tag find
+const addMounthName=heading_2[0];
 
-const heading_3 = document.getElementsByClassName('heading_3')// name h2 tag find
-const addAddress = heading_3[0]
+const heading_3 = document.getElementsByClassName('heading_3');// name h2 tag find
+const addAddress = heading_3[0];
 
-//first form submit data
+// first  form submition
 form.addEventListener('submit',(e)=>{
   e.preventDefault()
 
-  let massNameValue = name.value
-  addName.innerHTML = `Mass Name: ${massNameValue}`//add mass name 
-  localStorage.setItem('massNameValue', massNameValue)// passing data to views.js file
+  let massNameValue = name.value;
+  addName.innerHTML = `Mass Name: ${massNameValue}`;//add mass name 
+  localStorage.setItem('massNameValue', massNameValue);// passing data to views.js file
 
-  let mounthNameValue = monuthName.value
-  addMounthName.innerHTML = `Monuth Name: ${mounthNameValue}`//add mounth name
-  localStorage.setItem('mounthNameValue', mounthNameValue)// passing data to views.js file
+  let mounthNameValue = monuthName.value;
+  addMounthName.innerHTML = `Monuth Name: ${mounthNameValue}`;//add mounth name
+  localStorage.setItem('mounthNameValue', mounthNameValue);// passing data to views.js file
 
-  let addAddressValue = address.value
-  addAddress.innerHTML = `Address: ${addAddressValue}`
-  localStorage.setItem('addAddressValue', addAddressValue)// passing data to views.js file
+  let addAddressValue = address.value;
+  addAddress.innerHTML = `Address: ${addAddressValue}`;
+  localStorage.setItem('addAddressValue', addAddressValue);// passing data to views.js file
 
-  let managerNameValue = managerName.value
-  localStorage.setItem('managerNameValue', managerNameValue)// passing data to views.js file
-  form.reset()
+  let managerNameValue = managerName.value;
+  localStorage.setItem('managerNameValue', managerNameValue);// passing data to views.js file
+  form.reset();
 
   // window.location.href = 'views.html'
 })//add eventLiisterner in submit button
 
-
-
-
 // first form end here and second form start here
-const findForm = document.querySelectorAll('form')
-const form_2 = findForm[1]
+const findForm = document.querySelectorAll('form');
+const form_2 = findForm[1];
 
-const [tmcInput,tmInput,acInput,rcInput,bnInput,sbInput,wbInput,dbInput,bmInput,rmInput,bacInput] = form_2.querySelectorAll('input') //Destructuring 
+const [tmcInput,tmInput,acInput,rcInput] = form_2.querySelectorAll('input'); //Destructuring 
 
 
 
 //second form submition
 form_2.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  const tmcValue  = Math.abs(parseFloat(tmcInput.value)) // total mountly cost
-  const tmValue = Math.abs(parseFloat(tmInput.value)) // total meal 
-  const mc = parseFloat((tmcValue / tmValue).toFixed(2)) // calculate meal charge TMC/TM
-  localStorage.setItem('mc',mc) //passing data to views.js file
+  e.preventDefault();
+  const tmcValue  = Math.abs(parseInt(tmcInput.value));// total mountly cost
+  localStorage.setItem('tmcValue',tmcValue);
 
-  const bmValue = Math.abs(parseInt(bmInput.value))// boder meal
-  localStorage.setItem('bmValue',bmValue) 
-  const boderMealCost = Math.ceil(mc*bmValue)// calculate boder total meal cost
-  localStorage.setItem('boderMealCost',boderMealCost) //passing data to views.js file 
+  const tmValue = Math.abs(parseInt(tmInput.value)); // total meal 
+  localStorage.setItem('tmValue',tmValue);
+  const mc = parseFloat((tmcValue / tmValue).toFixed(4)); // calculate meal charge TMC/TM
+  localStorage.setItem('mc',mc);//passing data to views.js file 
+  console.log(mc);
+  window.location.href = 'views.html';
+  form_2.reset();
+})
 
-  const sbValue = Math.abs(parseInt(sbInput.value))// shaf bill
-  const wbValue = Math.abs(parseFloat(wbInput.value).toFixed(2))// wifi bill
-  const dbValue = Math.abs(parseFloat(dbInput.value).toFixed(2))// dust bill
+// third form submition
+const form_3 = findForm[2];
+const[bnInput,bmInput,mealCharge,sbInput,wbInput,dbInput,rmInput,bacInput] = form_3.querySelectorAll('input'); //Destructuring
  
 
-  const bacValue = Math.abs(parseInt(bacInput.value))// boder additional cost
-  let boderTotalCost 
-  
-  if(bacValue){
-     boderTotalCost = Math.ceil(sbValue+wbValue+dbValue+boderTotalCost+bacValue)
-    form_2.reset()
-  }
-  else{
-     boderTotalCost = Math.ceil(sbValue+wbValue+dbValue+boderTotalCost)  
-    form_2.reset()
-  }// calculate boder total cost 
+form_3.addEventListener('submit',(e)=>{
+  e.preventDefault();
 
-  localStorage.setItem('boderTotalCost', boderTotalCost)// passing data to views.js file
-  // window.location.href = 'views.html'
-  const bnValue = bnInput.value
+  const bnValue = bnInput.value //boder name 
+  localStorage.setItem('bnValue',bnValue);
+ 
+  const bmValue = parseInt(bmInput.value);// boder meal
+  localStorage.setItem('bmValue',bmValue); //passing data to views.js file
 
-  localStorage.setItem('bnValue',bnValue)
+
+  const mc = mealCharge.value;
+  const boderMealCost = Math.ceil(mc*bmValue);// calculate boder total meal cost
+  localStorage.setItem('boderMealCost',boderMealCost); //passing data to views.js file
+
+  const sbValue = parseInt(sbInput.value);// shaf bill
+  localStorage.setItem('sbVlaue',sbValue);
+
+  const wbValue = Math.abs(parseFloat(wbInput.value).toFixed(2));// wifi bill
+  localStorage.setItem('wbValue',wbValue);
+
+  const dbValue = Math.abs(parseFloat(dbInput.value).toFixed(2));// dust bill
+  localStorage.setItem('dbValue',dbValue);
+ 
+  const bacValue = Math.abs(parseInt(bacInput.value));// boder additional cost
+  localStorage.setItem('bacValue',bacValue);
+ 
+  const rmValue = parseInt(rmInput.value); // recive money 
+  localStorage.setItem('rmValue',rmValue);
   
-  
-  form_2.reset()
+  window.location.href = 'views.html';// show the value in views.html page
+  form_3.reset();
   
 })
 
--- views.js
+++ views.js
@@ -1,48 +1,110 @@
 
 
 
-const [div_1,div_2,div_3] = document.querySelectorAll('div') // distructing all div tag
+const [div_1,div_2,div_3] = document.querySelectorAll('div'); // distructing all div tag
 
-let [massName,mounthName,address] = div_1.querySelectorAll('h2')// distructing h2 tag from div_1
-const massNameValue = localStorage.getItem('massNameValue')// get mass name 
-massName.innerHTML = `Mass Name: ${massNameValue}`// set mass name in views.html
+let [massName,mounthName,address] = div_1.querySelectorAll('h2');// distructing h2 tag from div_1
+const massNameValue = localStorage.getItem('massNameValue');// get mass name 
+massName.innerHTML = `Mass Name: ${massNameValue}`;// set mass name in views.html
 
-const mounthNameValue = localStorage.getItem('mounthNameValue')//get mounth name 
-mounthName.innerHTML = `Mounth Name: ${mounthNameValue}`//set mounth in views.html
+const mounthNameValue = localStorage.getItem('mounthNameValue');//get mounth name 
+mounthName.innerHTML = `Mounth Name: ${mounthNameValue}`;//set mounth in views.html
 
-const addAddressValue = localStorage.getItem('addAddressValue')//get address 
-address.innerHTML = `Address: ${addAddressValue}`// set address in views.html
-
-
-let [managerName,mealCharge] = div_2.querySelectorAll('h2')//distructing h2 tag from div_2
-const managerNameValue = localStorage.getItem('managerNameValue')//get manager name
-managerName.innerHTML = `Manager Name: ${managerNameValue}`//set manager name in views.html
+const addAddressValue = localStorage.getItem('addAddressValue');//get address 
+address.innerHTML = `Address: ${addAddressValue}`;// set address in views.html
 
 
+let [managerName,mealCharge] = div_2.querySelectorAll('h2');//distructing h2 tag from div_2
+const managerNameValue = localStorage.getItem('managerNameValue');//get manager name
+managerName.innerHTML = `Manager Name: ${managerNameValue}`;//set manager name in views.html
+
+
+let h3 = div_2.querySelector('h3');
+const mc = localStorage.getItem('mc');
+h3.innerHTML = `${mc}`;
+const mealChargeValue = parseFloat(h3.textContent);
+
+
+let [totaMounthltCost,totalMeal] = div_3.querySelectorAll('h2');//distructing h2 tag from div_3
+const tmcValue = localStorage.getItem('tmcValue');
+totaMounthltCost.innerHTML = `Total Mountly Cost: ${tmcValue}`;
+
+const tmValue = localStorage.getItem('tmValue');
+totalMeal.innerHTML = `Total Meal: ${tmValue}`;
+
+
+// all value store in array
+let all = [];
  
-const boderTotalCost = localStorage.getItem('boderTotalCost')//get data from script.js 
-const bmValue = localStorage.getItem('bmValue')
-const bnValue = localStorage.getItem('bnValue')
-const boderMealCost = localStorage.getItem('boderMealCost')
-const tbody = document.querySelector('tbody')//select table body
+const bnValue = localStorage.getItem('bnValue'); // get value from script.js file
+all.push(bnValue);
 
-const tr = document.createElement('tr')//create tr tag 
-tbody.appendChild(tr) //append tr tag inside tbody tag
-const td1 = document.createElement('td')//create td tag
-td1.textContent = bnValue //add value in td tag
+const bmValue = parseInt(localStorage.getItem('bmValue'));
+all.push(bmValue);
 
-const td2 = document.createElement('td')
-td2.textContent = bmValue //add value in td tag
+const boderMealCost = Math.ceil(mealChargeValue*bmValue);
+all.push(boderMealCost);
 
-const td3 = document.createElement('td')
-td3.textContent = boderMealCost
+const sbValue = parseFloat(localStorage.getItem('sbVlaue')) || 0;
+all.push(sbValue);
 
+const wbValue = parseInt(localStorage.getItem('wbValue')) || 0;
+all.push(wbValue);
 
+const dbValue = parseInt(localStorage.getItem('dbValue')) || 0;
+all.push(dbValue);
 
-tr.appendChild(td1) //append td tag inside tr tag 
-tr.appendChild(td2) //append td tag inside tr tag
-tr.appendChild(td3) //append td tag inside tr tag
+const bacValue  = parseInt(localStorage.getItem('bacValue')) || 0;
 
+let boderTotalCost;
+if (bacValue){
+  boderTotalCost = boderMealCost + sbValue + wbValue + dbValue + bacValue;
+}
+else{
+  boderTotalCost = boderMealCost + sbValue + wbValue + dbValue;
+}
+all.push(boderTotalCost);// push boderTotalCost in all array
 
+const rmValue = parseInt(localStorage.getItem('rmValue'));
+all.push(rmValue);
 
+// check deu or returnMoney
+let deu = 0;
+let returnMoney = 0;
+if (boderTotalCost > rmValue) {
+  deu = boderTotalCost - rmValue;
+} 
+else if (boderTotalCost < rmValue) {
+  returnMoney = rmValue - boderTotalCost;
+}
+all.push(deu);
+all.push(returnMoney);
+all.push(bacValue);
+
+//insert data in table 
+for (let i = 0; i < all.length; i++) {
+  let tbody = document.querySelector('tbody');
+  let td = document.createElement('td');
+  td.innerHTML = all[i]
+  tbody.appendChild(td);
+}
+
+// print the views page
+const homeBtn = document.getElementById('homeBtn');
+const printBtn = document.getElementById('printBtn');
+const headingBtn = document.querySelector('h1')
+
+printBtn.addEventListener('click',(e)=>{
+  homeBtn.style.display = 'none';
+  printBtn.style.display = 'none';
+  headingBtn.style.display = 'none';
+  address.style.display = 'none';
+
+  window.print();
+
+  homeBtn.style.display = 'block';
+  printBtn.style.display = 'block';
+  headingBtn.style.display = 'block';
+  address.style.display = 'block';
+})// print the views page 
 
